d1a8a90425a77ab003fcd9e2fb0c0ff409456e0c
csiu <celia.sy.siu@gmail.com>
2017-03-02 16:41:30 -0800

    enable additional options

--- R/load_chromhmm_overlap_enrichments.R
+++ R/load_chromhmm_overlap_enrichments.R
@@ -4,9 +4,10 @@
 #' @param enrichment_file
 #'          ChormHMM model_*_emissions.txt file.
 #'          The first column of the TSV should be chromatin state.
+#' @param ... options for readr::read_tsv(...)
 #' @export
-load_chromhmm_overlap_enrichments <- function(enrichment_file) {
-  dat <- readr::read_tsv(enrichment_file, progress = FALSE)
+load_chromhmm_overlap_enrichments <- function(enrichment_file, ...) {
+  dat <- readr::read_tsv(enrichment_file, progress = FALSE, ...)
   colnames(dat)[1] <- "state"
   filter(dat, state != "Base")
 }
