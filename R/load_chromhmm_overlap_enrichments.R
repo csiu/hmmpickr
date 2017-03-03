22c356f9815abc24ef397e89f65c055a61899efa
csiu <celia.sy.siu@gmail.com>
2017-03-02 16:37:40 -0800

    Fix name error

--- R/load_chromhmm_overlap_enrichments.R
+++ R/load_chromhmm_overlap_enrichments.R
@@ -1,11 +1,11 @@
-#' Load ChromHMM enrichment file
+#' Load ChromHMM overlap enrichment file
 #' @details Replaces the name of the first column with "state" and
 #'          filters out the "Base" state.
 #' @param enrichment_file
 #'          ChormHMM model_*_emissions.txt file.
 #'          The first column of the TSV should be chromatin state.
 #' @export
-load_chromhmm_emissions <- function(enrichment_file) {
+load_chromhmm_overlap_enrichments <- function(enrichment_file) {
   dat <- readr::read_tsv(enrichment_file, progress = FALSE)
   colnames(dat)[1] <- "state"
   filter(dat, state != "Base")
