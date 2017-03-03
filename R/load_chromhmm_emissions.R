6ee50eccdce3024ff2ef420ef8cb44e3b901595b
csiu <celia.sy.siu@gmail.com>
2017-03-02 17:15:10 -0800

    Load emission probabilities

---
+++ R/load_chromhmm_emissions.R
@@ -0,0 +1,8 @@
+#' Load ChromHMM emission probabilities
+#' @param filename The ChromHMM model emissions file
+#' @param ... options for readr::read_tsv(...)
+load_chromhmm_emissions <- function(filename, ...) {
+  emissions_probs <- readr::read_tsv(filename, progress=FALSE, ...)
+  colnames(emissions_probs)[1] <- "state"
+  tidyr::gather(emissions_probs, mark, prob, -state)
+}
