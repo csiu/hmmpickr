4ae65cafc0fcd250ec78fde22f3e6ec23732a6a9
csiu <celia.sy.siu@gmail.com>
2017-03-02 17:17:09 -0800

    Might as well export this function as well

--- R/load_chromhmm_emissions.R
+++ R/load_chromhmm_emissions.R
@@ -1,6 +1,7 @@
 #' Load ChromHMM emission probabilities
 #' @param filename The ChromHMM model emissions file
 #' @param ... options for readr::read_tsv(...)
+#' @export
 load_chromhmm_emissions <- function(filename, ...) {
   emissions_probs <- readr::read_tsv(filename, progress=FALSE, ...)
   colnames(emissions_probs)[1] <- "state"
