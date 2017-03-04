70575999fe9d9f9f7393fded48f309a97ea31279
csiu <celia.sy.siu@gmail.com>
2017-03-03 21:13:09 -0800

    Make hide model path an option

--- R/hmmpick.R
+++ R/hmmpick.R
@@ -1,8 +1,10 @@
-#' Given a list of model files, chooses the best model
-#' @param model_files List of ChromHMM model files to choose from
+#' Pick the ChromHMM model that is most well-defined
+#' @param model_files A list of ChromHMM models
+#' @param full_path Boolean; TRUE to return the full model path in the output,
+#'                  or FALSE to return only the model basename.
 #' @format Returns a list sorted by ascending homogeneity cost.
-#'         The models are used as element names, and the
-#'         homogeneity cost are used as element values.
+#'         The models in \code{model_files} are used as the element names, and
+#'         the model homogeneity cost are used as the element values.
 #' @seealso \code{\link{homogeneity_cost}} for the calculation of the
 #'          homogeneity cost.
 #' @examples
@@ -15,6 +17,8 @@
 #' hmmpick(model_files)
 #' @import dplyr
 #' @export
-hmmpick <- function(model_files) {
-  sort(sapply(model_files, hmmpickr::homogeneity_cost))
+hmmpick <- function(model_files, full_path=FALSE) {
+  output <- sapply(model_files, hmmpickr::homogeneity_cost)
+  if (!full_path) names(output) <- basename(names(output))
+  sort(output)
 }
