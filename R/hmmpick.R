25ba6bd985f9e20565adcf23a5b533bc984392b3
csiu <celia.sy.siu@gmail.com>
2017-03-03 20:04:25 -0800

    include hmmpick function

---
+++ R/hmmpick.R
@@ -0,0 +1,20 @@
+#' Given a list of model files, chooses the best model
+#' @param model_files List of ChromHMM model files to choose from
+#' @format Returns a list sorted by ascending homogeneity cost.
+#'         The models are used as element names, and the
+#'         homogeneity cost are used as element values.
+#' @details See \code{?homogeneity_cost} for the calculation of the
+#'          homogeneity cost.
+#' @examples
+#' model_files <- c(
+#'   system.file("extdata", "model_roadmap15.txt",
+#'               package="hmmpickr"),
+#'   system.file("extdata", "model_roadmap18.txt",
+#'               package="hmmpickr")
+#' )
+#' hmmpick(model_files)
+#' @import dplyr
+#' @export
+hmmpick <- function(model_files) {
+  sort(sapply(model_files, hmmpickr::homogeneity_cost))
+}
